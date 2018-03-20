d32d6a962aca12751320f55bf3195c7f57084881
Seong ho Hong <grohong76@gmail.com>
2018-03-20 20:39:38 +0900

    NMK -> not solve

---
+++ Middle/GreedyAlgorism/src/main/java/NMK.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NMK {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+        int increaseLength = scanner.nextInt();
+        int decreaseLength = scanner.nextInt();
+
+        if (inputNumber < increaseLength + decreaseLength) {
+            System.out.println(-1);
+        } else {
+            if (increaseLength == 1) {
+                
+            }
+        }
+    }
+}
